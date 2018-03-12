9a6fe94759aa26332d7f7adfd83e6bee576c979b
John Rykken and Sara Hamilton <john@>
2018-03-12 14:29:50 -0700

    add linter and Entry class with two prototypes

--- src/journal.js
+++ src/journal.js
@@ -1,3 +1,21 @@
 export function Entry(title, body) {
-  // code here
+  this.title = title;
+  this.body = body;
+}
+
+Entry.prototype.wordCount = function() {
+  var bodyCount = this.body.split(" ").length;
+  return bodyCount;
+}
+
+Entry.prototype.vowelCount = function() {
+  var count = 0;
+  var vowels = ["a", "e", "i", "o", "u"];
+  var textArray = this.body.split("");
+  textArray.forEach(function(letter){
+    if(vowels.includes(letter)) {
+      count++;
+    }
+  });
+  return count;
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
     var title = $('#title').val();
     var content = $('#content').val();
     var entry = new Entry(title, content);
+    var entryWordCount = entry.wordCount();
+    var entryVowelCount = entry.vowelCount();
+    console.log('You made it this far. words ' + entryWordCount + " vowels " +  entryVowelCount);
 
   }); // submit
 }); // doc ready
